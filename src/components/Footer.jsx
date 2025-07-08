e958e193778d53dd4991259ea96b33a37e04970e
Abdullah Anwar <abdullah860259@gmail.com>
2025-07-08 08:04:48 -0700

    Edited Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-        <div className="footer text-sm bg-purple-300 fixed bottom-0 flex flex-col items-center justify-center  w-full h-[40px]"> 
+        <div className="footer text-sm bg-purple-300 fixed bottom-0 flex flex-col items-center justify-center   w-full h-[40px]"> 
            <p >Developed by Hafiz Abdullah Anwar</p>
-            <p  ></p>
+            <p > The site might be slow due to backend server issues </p>
            <p > Copyright &copy; 2025 || All Rights Reserved </p>
         </div>
     </>
